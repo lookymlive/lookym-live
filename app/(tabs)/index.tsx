883a6847dc3599a90206619ade8dabd19a2db20c
lookymlive <lookymlive@gmail.com>
2025-04-16 21:13:58 -0300

    refactor: replace HomeScreen implementation with VideoTestScreen for streamlined video testing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,38 +1,5 @@
-
-import { View, Text, StyleSheet, FlatList, SafeAreaView } from 'react-native';
-import { useVideoStore } from '@/store/video-store';
-import VideoThumbnail from '@/components/VideoThumbnail';
-import Header from '@/components/Header';
-import Stories from '@/components/Stories';
-
-import { useEffect } from 'react';
+import VideoTestScreen from '@/screens/VideoTestScreen';
 
 export default function HomeScreen() {
-  const { videos, fetchVideos } = useVideoStore();
-
-  useEffect(() => {
-    fetchVideos();
-  }, []);
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Header />
-      <FlatList
-        data={videos}
-        keyExtractor={(item) => item.id}
-        showsVerticalScrollIndicator={false}
-        ListHeaderComponent={<Stories />}
-        renderItem={({ item }) => (
-          <VideoThumbnail video={item} />
-        )}
-      />
-    </SafeAreaView>
-  );
+  return <VideoTestScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
