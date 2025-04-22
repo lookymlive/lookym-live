08d33ddac64f581abbd55198c5e677c8ff3149e1
lookymlive <lookymlive@gmail.com>
2025-04-22 16:52:55 -0300

    feat: add Jest configuration for testing TypeScript files and module resolution

---
+++ jest.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  roots: ['<rootDir>'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!(@expo|expo-constants|expo-modules-core|react-native|@react-native|expo-av)/)"
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
