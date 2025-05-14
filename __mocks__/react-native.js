43f260ff6f2bfc7a4bcffef0d1c3725ce0fa1704
lookymlive <lookymlive@gmail.com>
2025-05-14 16:37:37 -0300

    fix(mocks): enhance react-native mocks by adding Pressable and Alert

--- __mocks__/react-native.js
+++ __mocks__/react-native.js
@@ -1,8 +1,13 @@
 // __mocks__/react-native.js
 module.exports = {
-  StyleSheet: { create: (styles) => styles },
+  StyleSheet: {
+    create: (styles) => styles,
+    flatten: (style) => style,
+  },
   Text: "Text",
   View: "View",
   TouchableOpacity: "TouchableOpacity",
+  Pressable: "Pressable",
+  Alert: { alert: jest.fn() },
   // Puedes agregar otros mocks si los necesitas
 };
