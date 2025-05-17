cbd539a8fbc2293578d0d93c333c53d375bb166e
lookymlive <lookymlive@gmail.com>
2025-05-17 20:40:12 -0300

    refactor(mocks): replace static mocks with dynamic mock components
    Replace static string mocks with dynamically created mock components using `React.forwardRef` to better simulate React Native components in tests. This change improves the accuracy of component mocking and allows for more realistic testing scenarios.

--- __mocks__/react-native.js
+++ __mocks__/react-native.js
@@ -1,13 +1,28 @@
 // __mocks__/react-native.js
+const React = require("react");
+
+function createMockComponent(name) {
+  const Comp = React.forwardRef((props, ref) => {
+    return React.createElement(
+      "mock-" + name,
+      { ...props, ref },
+      props.children
+    );
+  });
+  Comp.displayName = name;
+  return Comp;
+}
+
 module.exports = {
   StyleSheet: {
     create: (styles) => styles,
     flatten: (style) => style,
   },
-  Text: "Text",
-  View: "View",
-  TouchableOpacity: "TouchableOpacity",
-  Pressable: "Pressable",
+  Text: createMockComponent("Text"),
+  View: createMockComponent("View"),
+  TouchableOpacity: createMockComponent("TouchableOpacity"),
+  Pressable: createMockComponent("Pressable"),
+  ActivityIndicator: createMockComponent("ActivityIndicator"),
   Alert: { alert: jest.fn() },
   // Puedes agregar otros mocks si los necesitas
 };
