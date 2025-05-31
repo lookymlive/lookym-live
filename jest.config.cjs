6d2f6b51ffbc5fc9d13e3e4b7ac0f97fea3ff978
lookymlive <lookymlive@gmail.com>
2025-05-31 20:50:16 -0300

    fix(jest.config.cjs): restore permissive transformIgnorePatterns for Expo/React Native ESM

--- jest.config.cjs
+++ jest.config.cjs
@@ -9,7 +9,8 @@ module.exports = {
     "^.+\\.(ts|tsx)$": "babel-jest",
   },
   transformIgnorePatterns: [
-    "node_modules/(?!(@expo|expo-constants|expo-modules-core|react-native|@react-native|expo-av)/)",
+    // Restaurar patrón más permisivo para Expo/React Native ESM (estado funcional anterior)
+    "node_modules/(?!(expo|@expo|expo-constants|expo-modules-core|expo-secure-store|react-native|@react-native|@react-native-async-storage|@react-navigation|@unimodules|@supabase|@react-native-community|react-native-svg|react-native-reanimated|react-native-gesture-handler|react-native-safe-area-context|react-native-screens|expo-av)/)",
   ],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
